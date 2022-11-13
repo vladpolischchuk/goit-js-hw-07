797c978e712e75e0e723d342b0489c178f44bc27
vladpolischchuk <105670228+vladpolischchuk@users.noreply.github.com>
2022-11-13 13:13:42 +0200

    creation of galleries

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+const items = [];
+
+galleryItems.forEach(element => {
+    const galleryItem = document.createElement('div');
+    galleryItem.className = 'gallery__item';
+  
+    const galleryLink = document.createElement('a');
+    galleryLink.className = 'gallery__link';
+    galleryLink.href = element.original;
+  
+    const galleryImage = document.createElement('img');
+    galleryImage.className = 'gallery__image';
+    galleryImage.src = element.preview;
+    galleryImage.setAttribute('data-source', element.original);
+    galleryImage.alt = element.description;
+
+    galleryItem.append(galleryLink);
+    galleryLink.append(galleryImage);
+    items.push(galleryItem);
+});
+
+gallery.append(...items);
+
+gallery.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    };
+
+    const selectedImg = e.target.getAttribute('data-source');
+
+    const instance = basicLightbox.create(`
+  <img src="${selectedImg}" width="1280" height="720">
+  `);
+
+    instance.show();
+
+    gallery.addEventListener('keydown', e => {
+        if (e.key === 'Escape') {
+            instance.close();
+        }
+    });
+});
+
 console.log(galleryItems);
